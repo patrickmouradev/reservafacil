6b30e42b36900df43773a0aa4a6fed169f8fbafc
Patrick Moura <patrickmoura@gmail.com>
2020-03-27 13:13:23 -0300

    Crinado Projeto

---
+++ src/main/java/br/com/cvc/ReservafacilApplication.java
@@ -0,0 +1,13 @@
+package br.com.cvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservafacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservafacilApplication.class, args);
+	}
+
+}
